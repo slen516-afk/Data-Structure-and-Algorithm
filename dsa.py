bb66b8444140b9779eb4bc3c641006ec2a7fc150
slen516-afk <slen516@gmail.com>
2026-01-29 07:51:33 +0800

    dsa

---
+++ dsa.py
@@ -0,0 +1,45 @@
+class Node:
+    """ArithmeticError: Raised when an error occurs in numeric calculations."""
+
+    data=None
+    next_node=None
+
+    def __init__(self, data):
+        self.data=data
+    
+    def __repr__(self):
+        return "<Node data: %s>" % self.data
+
+class LinkedList:
+    """
+    Singly Linked List 
+    """
+
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        return self.head == None
+
+    def size(self):
+        """
+        Returns the number of nodes in the list
+        Takes(O)n times
+        """
+        current = self.head
+        count = 0
+
+        while current:
+            count += 1
+            current = current.next_node
+        
+        return count
+    def add(self, data):
+        """
+        Adds new Node conotaining data at head of the list
+        Takes O(1) time
+
+        """
+        new_node = Node(data)
+        new_node.next_node = self.head
+        self.head = new_node
