72d3843c9ab60cb650e118d4c2c8961adc26be50
Ryan <slen516@gmail.com>
2026-01-30 15:09:02 +0800

    LinkedList

---
+++ Linked_list.py
@@ -0,0 +1,10 @@
+class Node:
+    """
+    An object for storing a single node of a linked list.
+    Models two attributes - data and the link to the next node in the list.
+    """
+    data = None
+    next_node = None
+
+    def __init__(self, data):
+        self.data = data
