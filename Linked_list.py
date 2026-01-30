07dab063c69f247698280181b8a9acf80cfb664b
Ryan <slen516@gmail.com>
2026-01-30 15:18:39 +0800

    0130 15:18

--- Linked_list.py
+++ Linked_list.py
@@ -8,3 +8,25 @@ class Node:
 
     def __init__(self, data):
         self.data = data
+
+
+    def __repr__(self):
+        return "<Node data: %s>" % self.data
+    
+class LinkedList:
+    """
+    Singly linked list
+    """
+
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        return self.head == None
+    
+    def size(self):
+        current = self.head
+        count = 0
+
+        
+
